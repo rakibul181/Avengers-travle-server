e313ac72d246708d95aa3508cc43c0eef7d29ddc
rakibul181 <rakibulhasan0641@gmail.com>
2022-11-10 20:28:05 +0600

    Node Config

--- index.js
+++ index.js
@@ -6,6 +6,21 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+require('dotenv').config()
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ip0tsyu.mongodb.net/?retryWrites=true&w=majority`;
+
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 
 app.get('/',(req, res)=>{
     res.send('Avengers Travel Is Running')
