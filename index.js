d98c933125b3703f90658ea5ac04aa87f14fb138
rakibul181 <rakibulhasan0641@gmail.com>
2022-11-11 15:28:41 +0600

    review data post on server

--- index.js
+++ index.js
@@ -18,29 +18,44 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 async function run() {
   const UserCollection = client.db("avengersTravel").collection("services");
+  const ReviewCollection = client.db("NodeMongoCRUD").collection("rewiews");
+
   try {
     app.post('/service', async (req, res) => {
       const service = req.body
       console.log(service);
-      const result = await UserCollection.insertOne(service)
+      const result = await  ReviewCollection.insertOne(service)
       res.send(result)
       console.log(result);
 
     })
-    app.get('/services', async(req, res)=>{
+    app.get('/services', async (req, res) => {
       const query = {}
       const cursor = UserCollection.find(query)
       const services = await cursor.toArray()
       res.send(services)
-  })
+    })
+    app.get('/3service', async (req, res) => {
+      const query = {}
+      const cursor = UserCollection.find(query)
+      const services = await cursor.limit(3).toArray()
+      res.send(services)
+    })
+    app.post('/reviews', async (req, res) => {
+      const review = req.body
+      console.log(review);
+      const result = await UserCollection.insertOne(review)
+      res.send(result)
+      console.log(result);
 
-  app.get('/service/:id', async(req, res)=>{
-    const id = req.params.id
-    const query = {_id: ObjectId(id)}
-    const service = await UserCollection.findOne(query)
-    res.send(service)
+    })
+    app.get('/service/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: ObjectId(id) }
+      const service = await UserCollection.findOne(query)
+      res.send(service)
 
-})
+    })
 
   }
   finally {
